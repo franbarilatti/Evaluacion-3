dc7941cf68e45adebaf3e9cad29123009c55a15b
franco <francoagustinbarilatti@hotmail.com>
2025-12-19 18:12:02 -0300

    primer commit y estructura inicial de microservicio de libros

---
+++ microservicio-libros/src/main/java/com/biblioteca/microservicio_libros/MicroservicioLibrosApplication.java
@@ -0,0 +1,13 @@
+package com.biblioteca.microservicio_libros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioLibrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioLibrosApplication.class, args);
+	}
+
+}
---
+++ microservicio-libros/src/test/java/com/biblioteca/microservicio_libros/MicroservicioLibrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biblioteca.microservicio_libros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioLibrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
