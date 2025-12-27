e104803ac92418ac399b1d1f1b5346755abb5dda
franco <francoagustinbarilatti@hotmail.com>
2025-12-27 09:20:20 -0300

    se creo el microservicio para prestamos

---
+++ microservicio-prestamos/src/main/java/com/biblioteca/prestamos/MicroservicioPrestamosApplication.java
@@ -0,0 +1,13 @@
+package com.biblioteca.prestamos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioPrestamosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioPrestamosApplication.class, args);
+	}
+
+}
---
+++ microservicio-prestamos/src/test/java/com/biblioteca/prestamos/MicroservicioPrestamosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biblioteca.prestamos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioPrestamosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
