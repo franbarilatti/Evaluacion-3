5a7d4721c5b5763a26fa7614d7e2b577aec2b0c2
franco <francoagustinbarilatti@hotmail.com>
2025-12-27 09:30:42 -0300

    feat(prestamos): se agrego la estructura de paquetes y se activo Feign

--- microservicio-prestamos/src/main/java/com/biblioteca/prestamos/MicroservicioPrestamosApplication.java
+++ microservicio-prestamos/src/main/java/com/biblioteca/prestamos/MicroservicioPrestamosApplication.java
@@ -2,8 +2,10 @@ package com.biblioteca.prestamos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class MicroservicioPrestamosApplication {
 
 	public static void main(String[] args) {
