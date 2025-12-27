0b6e1366d2236ebfe11d48e89dd181f2ba0ab96a
franco <francoagustinbarilatti@hotmail.com>
2025-12-27 09:56:53 -0300

    feat(prestamos): se creo LoanRepository

---
+++ microservicio-prestamos/src/main/java/com/biblioteca/prestamos/repository/LoanRepository.java
@@ -0,0 +1,17 @@
+package com.biblioteca.prestamos.repository;
+
+import com.biblioteca.prestamos.model.Loan;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface LoanRepository extends JpaRepository<Loan, Long> {
+
+    List<Loan> findByUserId(Long userId);
+
+    List<Loan> findByBookId(Long bookId);
+
+    List<Loan> findByReturnDateIsNull();
+}
