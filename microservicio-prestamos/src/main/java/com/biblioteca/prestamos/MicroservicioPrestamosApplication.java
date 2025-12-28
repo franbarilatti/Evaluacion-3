1b91f87dc00caa68d03c1d457f3f5db0f8f8a2b4
franco <francoagustinbarilatti@hotmail.com>
2025-12-28 09:55:09 -0300

    fix: se arreglo el proyecto para que no suba informacion sensible al repositorio

--- microservicio-prestamos/src/main/java/com/biblioteca/prestamos/MicroservicioPrestamosApplication.java
+++ microservicio-prestamos/src/main/java/com/biblioteca/prestamos/MicroservicioPrestamosApplication.java
@@ -2,13 +2,12 @@ package com.biblioteca.prestamos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients
 public class MicroservicioPrestamosApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(MicroservicioPrestamosApplication.class, args);
 	}
 
--- microservicio-prestamos/src/main/java/com/biblioteca/prestamos/config/FeignConfig.java
+++ microservicio-prestamos/src/main/java/com/biblioteca/prestamos/config/FeignConfig.java
@@ -2,10 +2,12 @@ package com.biblioteca.prestamos.config;
 
 import feign.Logger;
 import feign.okhttp.OkHttpClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableFeignClients(basePackages = "com.biblioteca.prestamos")
 public class FeignConfig {
 
     @Bean
